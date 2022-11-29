d5661e2d6e15f048fec3277a7349cf76b40cb64e
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-11-29 08:47:58 +0000

    Add revision tests

---
+++ tests/integrations/basic-fail/tests/ui_tests_bless.rs
@@ -0,0 +1,44 @@
+use std::num::NonZeroUsize;
+use ui_test::*;
+
+fn main() -> ui_test::color_eyre::Result<()> {
+    let path = "../../../target";
+    let mut config = Config {
+        quiet: false,
+        root_dir: "tests/actual_tests_bless".into(),
+        dependencies_crate_manifest_path: Some("Cargo.toml".into()),
+        output_conflict_handling: if std::env::var_os("BLESS").is_some() {
+            OutputConflictHandling::Bless
+        } else {
+            OutputConflictHandling::Error
+        },
+        // Make sure our tests are ordered for reliable output.
+        num_test_threads: NonZeroUsize::new(1).unwrap(),
+        ..Config::default()
+    };
+    config
+        .dependency_builder
+        .envs
+        .push(("CARGO_TARGET_DIR".into(), path.into()));
+
+    // hide binaries generated for successfully passing tests
+    let tmp_dir = tempfile::tempdir()?;
+    config.args.push("--out-dir".into());
+    config.args.push(tmp_dir.path().as_os_str().to_owned());
+
+    config.args.push("--edition=2021".into());
+    config.stderr_filter("in ([0-9]m )?[0-9\\.]+s", "");
+    config.stdout_filter("in ([0-9]m )?[0-9\\.]+s", "");
+    config.stderr_filter(r"[^ ]*/\.?cargo/registry/.*/", "$$CARGO_REGISTRY");
+    config.stderr_filter(
+        &std::path::Path::new(path)
+            .canonicalize()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .display()
+            .to_string(),
+        "$$DIR",
+    );
+    ui_test::run_tests(config)
+}
