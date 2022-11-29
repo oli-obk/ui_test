76cd0a9d9b86c5ae9356a356f477a402bac3148a
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-11-29 11:49:55 +0000

    Add test for attempting to add revisions to the `revisions` command

---
+++ tests/integrations/basic-fail/tests/actual_tests_bless/revised_revision.rs
@@ -0,0 +1,13 @@
+use basic_fail::add;
+//@[foo] revisions: foo bar
+
+#[cfg(foo)]
+fn main() {
+    add("42", 3); //~[foo] ERROR: mismatched types
+}
+
+#[cfg(bar)]
+fn main() {
+    add("42", 3);
+    //~[bar]^ ERROR: mismatched types
+}
