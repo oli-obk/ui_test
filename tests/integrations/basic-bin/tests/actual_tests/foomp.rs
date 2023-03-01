956875ca6fb337d175376000ae8f5a5c9bbbc0e6
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-03-01 11:38:50 +0000

    Normalize away platform differences

--- tests/integrations/basic-bin/tests/actual_tests/foomp.rs
+++ tests/integrations/basic-bin/tests/actual_tests/foomp.rs
@@ -1,7 +1,9 @@
 use basic_bin::add;
 //~^ ERROR: extern location for basic_bin is of an unknown type
-//~| ERROR: file name should be lib*.rlib or lib*.so
+//~| ERROR: file name should be
 //~| ERROR: can't find crate for `basic_bin`
+
+//@normalize-stderr-test: "(file name should be).*" -> "$1"
 
 fn main() {
     add("42", 3);
