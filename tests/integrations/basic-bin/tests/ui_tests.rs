ac3d1f7a3a52d3972e65f4a445dcc43c927edafa
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-06-02 10:16:08 +0000

    Automatically detect `#[test]` and `fn main()` to select the right crate type

--- tests/integrations/basic-bin/tests/ui_tests.rs
+++ tests/integrations/basic-bin/tests/ui_tests.rs
@@ -22,10 +22,10 @@ fn main() -> ui_test::color_eyre::Result<()> {
     config.stderr_filter("/target/[^/]+/debug", "/target/$$TRIPLE/debug");
     config.path_stderr_filter(&std::path::Path::new(path), "$DIR");
 
-    ui_test::run_tests_generic(
+    run_tests_generic(
         config,
-        |path| path.extension().map(|ext| ext == "rs").unwrap_or(false),
-        |_, _| None,
+        default_file_filter,
+        default_per_file_config,
         // Avoid github actions, as these would end up showing up in `Cargo.stderr`
         status_emitter::Text,
     )
--- tests/integrations/basic-fail/tests/ui_tests.rs
+++ tests/integrations/basic-fail/tests/ui_tests.rs
@@ -29,10 +29,10 @@ fn main() -> ui_test::color_eyre::Result<()> {
     config.stderr_filter(r"[^ ]*/\.?cargo/registry/.*/", "$$CARGO_REGISTRY");
     config.path_stderr_filter(&std::path::Path::new(path), "$DIR");
 
-    ui_test::run_tests_generic(
+    run_tests_generic(
         config,
-        |path| path.extension().map(|ext| ext == "rs").unwrap_or(false),
-        |_, _| None,
+        default_file_filter,
+        default_per_file_config,
         // Avoid github actions, as these would end up showing up in `Cargo.stderr`
         status_emitter::Text,
     )
