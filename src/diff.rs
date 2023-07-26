99b4eaba4b57d8332223fc4e43dce5d01f670433
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-07-26 12:09:55 +0000

    Remove a now-useless struct

--- src/diff.rs
+++ src/diff.rs
@@ -1,150 +1,133 @@
 use colored::*;
 use prettydiff::{basic::DiffOp, diff_lines, diff_words};
 
-#[derive(Default)]
-struct DiffState;
-
 /// How many lines of context are displayed around the actual diffs
 const CONTEXT: usize = 2;
 
-impl<'a> DiffState {
-    fn skip(&mut self, skipped_lines: &[&'a str]) {
-        // When the amount of skipped lines is exactly `CONTEXT * 2`, we already
-        // print all the context and don't actually skip anything.
-        match skipped_lines.len().checked_sub(CONTEXT * 2) {
-            Some(skipped @ 2..) => {
-                // Print an initial `CONTEXT` amount of lines.
-                for line in &skipped_lines[..CONTEXT] {
-                    eprintln!(" {line}");
-                }
-                eprintln!("... {skipped} lines skipped ...");
-                // Print `... n lines skipped ...` followed by the last `CONTEXT` lines.
-                for line in &skipped_lines[skipped + CONTEXT..] {
-                    eprintln!(" {line}");
-                }
+fn skip(skipped_lines: &[&str]) {
+    // When the amount of skipped lines is exactly `CONTEXT * 2`, we already
+    // print all the context and don't actually skip anything.
+    match skipped_lines.len().checked_sub(CONTEXT * 2) {
+        Some(skipped @ 2..) => {
+            // Print an initial `CONTEXT` amount of lines.
+            for line in &skipped_lines[..CONTEXT] {
+                eprintln!(" {line}");
             }
-            _ => {
-                // Print all the skipped lines if the amount of context desired is less than the amount of lines
-                for line in skipped_lines {
-                    eprintln!(" {line}");
-                }
+            eprintln!("... {skipped} lines skipped ...");
+            // Print `... n lines skipped ...` followed by the last `CONTEXT` lines.
+            for line in &skipped_lines[skipped + CONTEXT..] {
+                eprintln!(" {line}");
+            }
+        }
+        _ => {
+            // Print all the skipped lines if the amount of context desired is less than the amount of lines
+            for line in skipped_lines {
+                eprintln!(" {line}");
             }
         }
     }
+}
 
-    fn print_left(&self, l: &str) {
-        eprintln!("{}{}", "-".red(), l.red());
-    }
-
-    fn print_right(&self, r: &str) {
-        eprintln!("{}{}", "+".green(), r.green());
-    }
-
-    fn row(&mut self, row: DiffOp<&'a str>) {
-        use prettydiff::basic::DiffOp::*;
-        match row {
-            Remove(l) => {
-                for l in l {
-                    self.print_left(l);
+fn row(row: DiffOp<&str>) {
+    use prettydiff::basic::DiffOp::*;
+    match row {
+        Remove(l) => {
+            for l in l {
+                eprintln!("{}{}", "-".red(), l.red());
+            }
+        }
+        Equal(l) => {
+            skip(l);
+        }
+        Replace(l, r) => {
+            for (l, r) in l.iter().zip(r) {
+                let diff = diff_words(l, r);
+                let diff = diff.diff();
+                let mut seen_l = false;
+                let mut seen_r = false;
+                for char in &diff {
+                    let is_whitespace =
+                        |s: &[&str]| s.iter().any(|s| s.chars().any(|s| s.is_whitespace()));
+                    match char {
+                        Insert(l) if !is_whitespace(l) => seen_l = true,
+                        Remove(r) if !is_whitespace(r) => seen_r = true,
+                        Replace(l, r) if !is_whitespace(l) && !is_whitespace(r) => {
+                            seen_l = true;
+                            seen_r = true;
+                            break;
+                        }
+                        _ => {}
+                    }
                 }
-            }
-            Equal(l) => {
-                self.skip(l);
-            }
-            Replace(l, r) => {
-                for (l, r) in l.iter().zip(r) {
-                    let diff = diff_words(l, r);
-                    let diff = diff.diff();
-                    let mut seen_l = false;
-                    let mut seen_r = false;
+                if seen_l && seen_r {
+                    // The line both adds and removes chars, print both lines, but highlight their differences instead of
+                    // drawing the entire line in red/green.
+                    eprint!("{}", "-".red());
                     for char in &diff {
-                        let is_whitespace =
-                            |s: &[&str]| s.iter().any(|s| s.chars().any(|s| s.is_whitespace()));
+                        match *char {
+                            Replace(l, _) | Remove(l) => {
+                                for l in l {
+                                    eprint!("{}", l.to_string().on_red())
+                                }
+                            }
+                            Insert(_) => {}
+                            Equal(l) => {
+                                for l in l {
+                                    eprint!("{l}")
+                                }
+                            }
+                        }
+                    }
+                    eprintln!();
+                    eprint!("{}", "+".green());
+                    for char in diff {
                         match char {
-                            Insert(l) if !is_whitespace(l) => seen_l = true,
-                            Remove(r) if !is_whitespace(r) => seen_r = true,
-                            Replace(l, r) if !is_whitespace(l) && !is_whitespace(r) => {
-                                seen_l = true;
-                                seen_r = true;
-                                break;
+                            Remove(_) => {}
+                            Replace(_, r) | Insert(r) => {
+                                for r in r {
+                                    eprint!("{}", r.to_string().on_green())
+                                }
                             }
-                            _ => {}
+                            Equal(r) => {
+                                for r in r {
+                                    eprint!("{r}")
+                                }
+                            }
                         }
                     }
-                    if seen_l && seen_r {
-                        // The line both adds and removes chars, print both lines, but highlight their differences instead of
-                        // drawing the entire line in red/green.
-                        eprint!("{}", "-".red());
-                        for char in &diff {
-                            match *char {
-                                Replace(l, _) | Remove(l) => {
-                                    for l in l {
-                                        eprint!("{}", l.to_string().on_red())
-                                    }
-                                }
-                                Insert(_) => {}
-                                Equal(l) => {
-                                    for l in l {
-                                        eprint!("{l}")
-                                    }
+                    eprintln!();
+                } else {
+                    // The line only adds or only removes chars, print a single line highlighting their differences.
+                    eprint!("{}", "~".yellow());
+                    for char in diff {
+                        match char {
+                            Remove(l) => {
+                                for l in l {
+                                    eprint!("{}", l.to_string().on_red())
                                 }
                             }
-                        }
-                        eprintln!();
-                        eprint!("{}", "+".green());
-                        for char in diff {
-                            match char {
-                                Remove(_) => {}
-                                Replace(_, r) | Insert(r) => {
-                                    for r in r {
-                                        eprint!("{}", r.to_string().on_green())
-                                    }
-                                }
-                                Equal(r) => {
-                                    for r in r {
-                                        eprint!("{r}")
-                                    }
+                            Equal(w) => {
+                                for w in w {
+                                    eprint!("{w}")
                                 }
                             }
-                        }
-                        eprintln!();
-                    } else {
-                        // The line only adds or only removes chars, print a single line highlighting their differences.
-                        eprint!("{}", "~".yellow());
-                        for char in diff {
-                            match char {
-                                Remove(l) => {
-                                    for l in l {
-                                        eprint!("{}", l.to_string().on_red())
-                                    }
+                            Insert(r) => {
+                                for r in r {
+                                    eprint!("{}", r.to_string().on_green())
                                 }
-                                Equal(w) => {
-                                    for w in w {
-                                        eprint!("{w}")
-                                    }
-                                }
-                                Insert(r) => {
-                                    for r in r {
-                                        eprint!("{}", r.to_string().on_green())
-                                    }
-                                }
-                                Replace(..) => unreachable!(),
                             }
+                            Replace(..) => unreachable!(),
                         }
-                        eprintln!();
                     }
-                }
-            }
-            Insert(r) => {
-                for r in r {
-                    self.print_right(r);
+                    eprintln!();
                 }
             }
         }
-    }
-
-    fn finish(self) {
-        eprintln!()
+        Insert(r) => {
+            for r in r {
+                eprintln!("{}{}", "+".green(), r.green());
+            }
+        }
     }
 }
 
@@ -163,9 +146,8 @@ pub fn print_diff(expected: &[u8], actual: &[u8]) {
     let expected_str = expected_str.replace(pat, "░");
     let actual_str = actual_str.replace(pat, "░");
 
-    let mut state = DiffState::default();
-    for row in diff_lines(&expected_str, &actual_str).diff() {
-        state.row(row);
+    for r in diff_lines(&expected_str, &actual_str).diff() {
+        row(r);
     }
-    state.finish();
+    eprintln!()
 }
