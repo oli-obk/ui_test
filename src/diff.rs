af68ee5d394f1a23f6c6ee02b71d978ca597739b
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-07-26 11:53:46 +0000

    Fully move to `prettydiff` instead of `diff`, as it has various convenience features that simplify our code

--- src/diff.rs
+++ src/diff.rs
@@ -1,73 +1,34 @@
 use colored::*;
-use diff::{chars, lines, Result, Result::*};
+use prettydiff::{basic::DiffOp, diff_lines, diff_words};
 
 #[derive(Default)]
-struct DiffState<'a> {
-    /// Whether we've already printed something, so we should print starting context, too.
-    print_start_context: bool,
-    /// When we skip lines, remember the last `CONTEXT` ones to
-    /// display after the "skipped N lines" message
-    skipped_lines: Vec<&'a str>,
-    /// When we see a removed line, we don't print it, we
-    /// keep it around to compare it with the next added line.
-    prev_left: Option<&'a str>,
-}
+struct DiffState;
 
 /// How many lines of context are displayed around the actual diffs
 const CONTEXT: usize = 2;
 
-impl<'a> DiffState<'a> {
-    /// Print `... n lines skipped ...` followed by the last `CONTEXT` lines.
-    fn print_end_skip(&self, skipped: usize) {
-        self.print_skipped_msg(skipped);
-        for line in self.skipped_lines.iter().rev().take(CONTEXT).rev() {
-            eprintln!(" {line}");
-        }
-    }
-
-    fn print_skipped_msg(&self, skipped: usize) {
-        match skipped {
-            // When the amount of skipped lines is exactly `CONTEXT * 2`, we already
-            // print all the context and don't actually skip anything.
-            0 => {}
-            // Instead of writing a line saying we skipped one line, print that one line
-            1 => eprintln!(" {}", self.skipped_lines[CONTEXT]),
-            _ => eprintln!("... {skipped} lines skipped ..."),
-        }
-    }
-
-    /// Print an initial `CONTEXT` amount of lines.
-    fn print_start_skip(&self) {
-        for line in self.skipped_lines.iter().take(CONTEXT) {
-            eprintln!(" {line}");
-        }
-    }
-
-    fn print_skip(&mut self) {
-        let half = self.skipped_lines.len() / 2;
-        if !self.print_start_context {
-            self.print_start_context = true;
-            self.print_end_skip(self.skipped_lines.len().saturating_sub(CONTEXT));
-        } else if half < CONTEXT {
-            // Print all the skipped lines if the amount of context desired is less than the amount of lines
-            for line in self.skipped_lines.drain(..) {
-                eprintln!(" {line}");
+impl<'a> DiffState {
+    fn skip(&mut self, skipped_lines: &[&'a str]) {
+        // When the amount of skipped lines is exactly `CONTEXT * 2`, we already
+        // print all the context and don't actually skip anything.
+        match skipped_lines.len().checked_sub(CONTEXT * 2) {
+            Some(skipped @ 2..) => {
+                // Print an initial `CONTEXT` amount of lines.
+                for line in &skipped_lines[..CONTEXT] {
+                    eprintln!(" {line}");
+                }
+                eprintln!("... {skipped} lines skipped ...");
+                // Print `... n lines skipped ...` followed by the last `CONTEXT` lines.
+                for line in &skipped_lines[skipped + CONTEXT..] {
+                    eprintln!(" {line}");
+                }
             }
-        } else {
-            self.print_start_skip();
-            let skipped = self.skipped_lines.len() - CONTEXT * 2;
-            self.print_end_skip(skipped);
-        }
-        self.skipped_lines.clear();
-    }
-
-    fn skip(&mut self, line: &'a str) {
-        self.skipped_lines.push(line);
-    }
-
-    fn print_prev(&mut self) {
-        if let Some(l) = self.prev_left.take() {
-            self.print_left(l);
+            _ => {
+                // Print all the skipped lines if the amount of context desired is less than the amount of lines
+                for line in skipped_lines {
+                    eprintln!(" {line}");
+                }
+            }
         }
     }
 
@@ -79,28 +40,34 @@ impl<'a> DiffState<'a> {
         eprintln!("{}{}", "+".green(), r.green());
     }
 
-    fn row(&mut self, row: Result<&'a str>) {
+    fn row(&mut self, row: DiffOp<&'a str>) {
+        use prettydiff::basic::DiffOp::*;
         match row {
-            Left(l) => {
-                self.print_skip();
-                self.print_prev();
-                self.prev_left = Some(l);
+            Remove(l) => {
+                for l in l {
+                    self.print_left(l);
+                }
             }
-            Both(l, _) => {
-                self.print_prev();
+            Equal(l) => {
                 self.skip(l);
             }
-            Right(r) => {
-                // When there's an added line after a removed line, we'll want to special case some print cases.
-                // FIXME(oli-obk): also do special printing modes when there are multiple lines that only have minor changes.
-                if let Some(l) = self.prev_left.take() {
-                    let diff = chars(l, r);
+            Replace(l, r) => {
+                for (l, r) in l.iter().zip(r) {
+                    let diff = diff_words(l, r);
+                    let diff = diff.diff();
                     let mut seen_l = false;
                     let mut seen_r = false;
                     for char in &diff {
+                        let is_whitespace =
+                            |s: &[&str]| s.iter().any(|s| s.chars().any(|s| s.is_whitespace()));
                         match char {
-                            Left(l) if !l.is_whitespace() => seen_l = true,
-                            Right(r) if !r.is_whitespace() => seen_r = true,
+                            Insert(l) if !is_whitespace(l) => seen_l = true,
+                            Remove(r) if !is_whitespace(r) => seen_r = true,
+                            Replace(l, r) if !is_whitespace(l) && !is_whitespace(r) => {
+                                seen_l = true;
+                                seen_r = true;
+                                break;
+                            }
                             _ => {}
                         }
                     }
@@ -110,18 +77,34 @@ impl<'a> DiffState<'a> {
                         eprint!("{}", "-".red());
                         for char in &diff {
                             match *char {
-                                Left(l) => eprint!("{}", l.to_string().on_red()),
-                                Right(_) => {}
-                                Both(l, _) => eprint!("{l}"),
+                                Replace(l, _) | Remove(l) => {
+                                    for l in l {
+                                        eprint!("{}", l.to_string().on_red())
+                                    }
+                                }
+                                Insert(_) => {}
+                                Equal(l) => {
+                                    for l in l {
+                                        eprint!("{l}")
+                                    }
+                                }
                             }
                         }
                         eprintln!();
                         eprint!("{}", "+".green());
                         for char in diff {
                             match char {
-                                Left(_) => {}
-                                Right(r) => eprint!("{}", r.to_string().on_green()),
-                                Both(l, _) => eprint!("{l}"),
+                                Remove(_) => {}
+                                Replace(_, r) | Insert(r) => {
+                                    for r in r {
+                                        eprint!("{}", r.to_string().on_green())
+                                    }
+                                }
+                                Equal(r) => {
+                                    for r in r {
+                                        eprint!("{r}")
+                                    }
+                                }
                             }
                         }
                         eprintln!();
@@ -130,15 +113,30 @@ impl<'a> DiffState<'a> {
                         eprint!("{}", "~".yellow());
                         for char in diff {
                             match char {
-                                Left(l) => eprint!("{}", l.to_string().on_red()),
-                                Both(l, _) => eprint!("{l}"),
-                                Right(r) => eprint!("{}", r.to_string().on_green()),
+                                Remove(l) => {
+                                    for l in l {
+                                        eprint!("{}", l.to_string().on_red())
+                                    }
+                                }
+                                Equal(w) => {
+                                    for w in w {
+                                        eprint!("{w}")
+                                    }
+                                }
+                                Insert(r) => {
+                                    for r in r {
+                                        eprint!("{}", r.to_string().on_green())
+                                    }
+                                }
+                                Replace(..) => unreachable!(),
                             }
                         }
                         eprintln!();
                     }
-                } else {
-                    self.print_skip();
+                }
+            }
+            Insert(r) => {
+                for r in r {
                     self.print_right(r);
                 }
             }
@@ -146,8 +144,6 @@ impl<'a> DiffState<'a> {
     }
 
     fn finish(self) {
-        self.print_start_skip();
-        self.print_skipped_msg(self.skipped_lines.len().saturating_sub(CONTEXT));
         eprintln!()
     }
 }
@@ -168,7 +164,7 @@ pub fn print_diff(expected: &[u8], actual: &[u8]) {
     let actual_str = actual_str.replace(pat, "░");
 
     let mut state = DiffState::default();
-    for row in lines(&expected_str, &actual_str) {
+    for row in diff_lines(&expected_str, &actual_str).diff() {
         state.row(row);
     }
     state.finish();
