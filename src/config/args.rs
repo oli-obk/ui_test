8bfd27319eabc126198d6788726a136c8b87e1a9
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-08-03 12:48:26 +0000

    Remove `clap` dependency

--- src/config/args.rs
+++ src/config/args.rs
@@ -1,36 +1,39 @@
 //! Default argument processing when `ui_test` is used
 //! as a test driver.
 
-use clap::Parser;
-
 /// Plain arguments if `ui_test` is used as a binary.
-#[derive(Parser, Debug, Clone)]
-#[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Filters that will be used to match on individual tests
     pub filters: Vec<String>,
 
     /// Whether to minimize output given to the user.
-    #[arg(short, long, default_value_t = false)]
     pub quiet: bool,
 
     /// Whether error on mismatches between `.stderr` files and actual
     /// output. Will update the files otherwise.
-    #[arg(long, default_value_t = false)]
     pub check: bool,
 }
 
-impl Default for Args {
-    fn default() -> Self {
-        Self::parse_from([std::env::current_exe().unwrap()])
-    }
-}
-
 impl Args {
-    /// Arguments if `ui_test` is used as a `cargo test`, so we need
-    /// to skip a level of `--` in order to not pick up `cargo`'s test
-    /// flags.
+    /// Arguments if `ui_test` is used as a `harness=false` test, called from `cargo test`.
     pub fn test() -> Self {
-        Args::parse_from(std::env::args().skip_while(|arg| arg != "--"))
+        let mut args = Args {
+            filters: vec![],
+            quiet: false,
+            check: false,
+        };
+        for arg in std::env::args().skip(1) {
+            if arg == "--" {
+                continue;
+            }
+            if arg == "--quiet" {
+                args.quiet = true;
+            } else if arg == "--check" {
+                args.check = true;
+            } else {
+                args.filters.push(arg);
+            }
+        }
+        args
     }
 }
