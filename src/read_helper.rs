259539541cbda4a41204cc887822f56e8ce6cd76
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-08-04 15:23:37 +0000

    We need to drop the `read`, so it gets closed correctly

--- src/read_helper.rs
+++ src/read_helper.rs
@@ -2,26 +2,31 @@ use bstr::ByteSlice;
 use std::io::Read;
 
 pub struct ReadHelper<T> {
-    read: T,
+    read: Option<T>,
     buf: Vec<u8>,
 }
 
 impl<T> From<T> for ReadHelper<T> {
     fn from(read: T) -> Self {
-        Self { read, buf: vec![] }
+        Self {
+            read: Some(read),
+            buf: vec![],
+        }
     }
 }
 
 impl<T> Drop for ReadHelper<T> {
     fn drop(&mut self) {
-        panic!("you need to `read_to_end` a `ReadHelper`")
+        if self.read.is_some() {
+            panic!("you need to `read_to_end` a `ReadHelper`")
+        }
     }
 }
 
 impl<T: Read> ReadHelper<T> {
     pub fn last_line(&mut self) -> &[u8] {
         let mut buf = [0; 100];
-        while let Ok(n @ 1..) = self.read.read(&mut buf) {
+        while let Ok(n @ 1..) = self.read.as_mut().unwrap().read(&mut buf) {
             self.buf.extend(&buf[..n]);
             if n != 100 {
                 break;
@@ -37,9 +42,11 @@ impl<T: Read> ReadHelper<T> {
             .unwrap_or(&[])
     }
     pub fn read_to_end(mut self) -> Vec<u8> {
-        self.read.read_to_end(&mut self.buf).unwrap();
-        let buf = std::mem::take(&mut self.buf);
-        std::mem::forget(self);
-        buf
+        self.read
+            .take()
+            .unwrap()
+            .read_to_end(&mut self.buf)
+            .unwrap();
+        std::mem::take(&mut self.buf)
     }
 }
