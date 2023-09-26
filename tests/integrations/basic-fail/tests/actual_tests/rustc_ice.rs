5e1d6b7d76591580114504d248fadc193ec71939
Ralf Jung <post@ralfj.de>
2023-09-26 08:39:22 +0200

    fix 'cargo test'

--- tests/integrations/basic-fail/tests/actual_tests/rustc_ice.rs
+++ tests/integrations/basic-fail/tests/actual_tests/rustc_ice.rs
@@ -1,7 +1,7 @@
 //@compile-flags: -Ztreat-err-as-bug
 //@rustc-env: RUSTC_BOOTSTRAP=1
 //@normalize-stderr-test: " +[0-9]+: .*\n" -> ""
-//@normalize-stderr-test: "                +at /.*\n" -> ""
+//@normalize-stderr-test: "                +at \.?/.*\n" -> ""
 //@normalize-stderr-test: " running on .*" -> ""
 //@normalize-stderr-test: "note: rustc 1\..*" -> ""
 use basic_fail::add;
