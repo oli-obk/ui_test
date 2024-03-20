dda7650b64849fabacd31e18e63a08ca122abac2
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-03-20 10:41:02 +0000

    will this break windows again?

--- tests/integrations/basic-bin/tests/actual_tests/foomp.rs
+++ tests/integrations/basic-bin/tests/actual_tests/foomp.rs
@@ -1,5 +1,9 @@
+//@normalize-stderr-test: "(file name should be).*" -> "$1"
+
 use basic_bin::add;
-//~^ ERROR: unresolved import `basic_bin`
+//~^ ERROR: can't find crate for `basic_bin`
+//~| ERROR: file name should be
+//~| ERROR: extern location for basic_bin is of an unknown type
 
 fn main() {
     add("42", 3);
